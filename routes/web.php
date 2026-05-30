07612820bcaf04337610e5e4ef1dd540617933a8
hannahelisha <hannahelisha.delacruz@cvsu.edu.ph>
2026-05-30 17:19:57 +0800

    Add root route redirect to login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+// Root Route
+Route::get('/', function () {
+    return redirect('/login');
+});
+
 // Register Routes
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
